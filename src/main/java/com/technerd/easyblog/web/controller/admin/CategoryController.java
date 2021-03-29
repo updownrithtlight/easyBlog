53f2943a53158a4b0e56103ca9a4b13e411cebd0
990766231 <990766231@qq.com>
2021-03-29 20:50:12 +0800

    sw

--- src/main/java/com/technerd/easyblog/web/controller/admin/CategoryController.java
+++ src/main/java/com/technerd/easyblog/web/controller/admin/CategoryController.java
@@ -2,6 +2,8 @@ package com.technerd.easyblog.web.controller.admin;
 import com.technerd.easyblog.service.CategoryService;
 import com.technerd.easyblog.utils.LocaleMessageUtil;
 import com.technerd.easyblog.web.controller.common.BaseController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @Controller
 @RequestMapping(value = "/admin/category")
+@Api(value = "分类")
 public class CategoryController extends BaseController {
 
     @Autowired
@@ -33,6 +36,7 @@ public class CategoryController extends BaseController {
      * @return 模板路径admin/admin_category
      */
     @GetMapping
+    @ApiOperation(value = "v")
     public String categories() {
 
 
