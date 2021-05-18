db1eb59091d9ccdfe22bad59c4c74b7b4d384a68
990766231 <990766231@qq.com>
2021-05-18 17:45:11 +0800

    13

--- src/main/java/com/technerd/easyblog/config/JwtInterptorConfig.java
+++ src/main/java/com/technerd/easyblog/config/JwtInterptorConfig.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -46,5 +47,13 @@ public class JwtInterptorConfig extends WebMvcConfigurationSupport{
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
 
 }
